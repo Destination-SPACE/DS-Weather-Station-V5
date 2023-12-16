b64d5aa99b9fd3c41311c0ada0c9235cbd693bdd
DeepSpace <deepspace00@pm.me>
2023-12-16 17:39:03 -0500

    added code for NeoPixels

--- software/Blinky/src/main.cpp
+++ software/Blinky/src/main.cpp
@@ -1,18 +1,31 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define NEO_PIXEL_DEFAULT PIN_NEOPIXEL
+#define NEO_PIXEL_WEATHER_WING 10
+#define DELAY_VALUE 500
+
+int led = LED_BUILTIN;
+
+Adafruit_NeoPixel PIXEL_BUILTIN(1, NEO_PIXEL_DEFAULT);
+Adafruit_NeoPixel PIXEL_WEATHER_WING(1, NEO_PIXEL_WEATHER_WING, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(led, OUTPUT);
+  PIXEL_BUILTIN.begin();
+  PIXEL_WEATHER_WING.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  PIXEL_BUILTIN.clear();
+  PIXEL_WEATHER_WING.clear();
+  
+  PIXEL_BUILTIN.setPixelColor(0, PIXEL_BUILTIN.Color(0, 150, 0));
+  PIXEL_WEATHER_WING.setPixelColor(0, PIXEL_WEATHER_WING.Color(0, 150, 0));
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(led, HIGH);
+  PIXEL_BUILTIN.show();
+  PIXEL_WEATHER_WING.show();
+  
+  delay(DELAY_VALUE);
 }
