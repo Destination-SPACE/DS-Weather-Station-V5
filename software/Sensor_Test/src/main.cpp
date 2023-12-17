62985dfc3b784fac080d6891445d04f9754ca627
DeepSpace <deepspace00@pm.me>
2023-12-16 21:56:23 -0500

    added MIT license agreement and header

--- software/Sensor_Test/src/main.cpp
+++ software/Sensor_Test/src/main.cpp
@@ -1,3 +1,35 @@
+/*******************************************************************************
+DESTINATION WEATHER FEATHER WING EXAMPLE - SENSOR TEST
+
+This is an example sketch for the Destination Weather FeatherWing remote sensing
+platform to test the SHT41 humidity & temperature sensor and the LPS22 pressure
+sensor.
+
+modified 2023-12-12
+by Madison Gleydura
+
+LICENSE AGREEMENT
+
+Copyright © 2023 Destination SPACE Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the “Software”), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*******************************************************************************/
+
 #include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_LPS2X.h>
