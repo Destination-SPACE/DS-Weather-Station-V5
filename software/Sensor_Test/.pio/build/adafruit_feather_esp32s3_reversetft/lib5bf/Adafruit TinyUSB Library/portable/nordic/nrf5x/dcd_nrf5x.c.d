186954a479f57db435e9f67793c8c2853c76d3f4
DeepSpace <deepspace00@pm.me>
2024-02-12 14:32:52 -0500

    something

---
+++ software/Sensor_Test/.pio/build/adafruit_feather_esp32s3_reversetft/lib5bf/Adafruit TinyUSB Library/portable/nordic/nrf5x/dcd_nrf5x.c.d
@@ -0,0 +1,8 @@
+.pio/build/adafruit_feather_esp32s3_reversetft/lib5bf/Adafruit\ TinyUSB\ Library/portable/nordic/nrf5x/dcd_nrf5x.c.o: \
+ .pio/libdeps/adafruit_feather_esp32s3_reversetft/Adafruit\ TinyUSB\ Library/src/portable/nordic/nrf5x/dcd_nrf5x.c \
+ .pio/libdeps/adafruit_feather_esp32s3_reversetft/Adafruit\ TinyUSB\ Library/src/tusb_option.h \
+ .pio/libdeps/adafruit_feather_esp32s3_reversetft/Adafruit\ TinyUSB\ Library/src/common/tusb_compiler.h \
+ .pio/libdeps/adafruit_feather_esp32s3_reversetft/Adafruit\ TinyUSB\ Library/src/tusb_config.h \
+ C:/Users/madis/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/qio_qspi/include/sdkconfig.h \
+ c:\users\madis\.platformio\packages\framework-arduinoespressif32\tools\sdk\esp32s3\include\arduino_tinyusb\include\tusb_config.h \
+ .pio/libdeps/adafruit_feather_esp32s3_reversetft/Adafruit\ TinyUSB\ Library/src/common/tusb_mcu.h
---
+++ software/Sensor_Test/.pio/build/adafruit_feather_esp32s3_reversetft/lib62e/Sensirion Core/SensirionRxFrame.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/adafruit_feather_esp32s3_reversetft/lib62e/Sensirion\ Core/SensirionRxFrame.cpp.o: \
+ .pio/libdeps/adafruit_feather_esp32s3_reversetft/Sensirion\ Core/src/SensirionRxFrame.cpp \
+ .pio/libdeps/adafruit_feather_esp32s3_reversetft/Sensirion\ Core/src/SensirionRxFrame.h \
+ .pio/libdeps/adafruit_feather_esp32s3_reversetft/Sensirion\ Core/src/SensirionErrors.h
