d2cd2f2821dc65add70578a62dd6867f8f621e33
DeepSpace <deepspace00@pm.me>
2024-04-10 14:02:24 -0400

    fixed destination again

--- software/Visual-Studio-Code-Installers/windows-vs-code-installer.ps1
+++ software/Visual-Studio-Code-Installers/windows-vs-code-installer.ps1
@@ -1,7 +1,7 @@
 $Username = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 
 $FileUri = "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64"
-$Destination = "downloads/vscodeInstaller.exe"
+$Destination = "vscodeInstaller.exe"
 $exeArgs = '/verysilent /tasks=addcontextmenufiles,addcontextmenufolders,addtopath'
 
 $bitsJobObj = Start-BitsTransfer $FileUri -Destination $Destination
