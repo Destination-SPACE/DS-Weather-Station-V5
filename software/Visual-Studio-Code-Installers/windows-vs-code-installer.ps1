3644a828298bbd300d19ea19f91bac38e67ba09e
DeepSpace <deepspace00@pm.me>
2024-04-10 14:24:49 -0400

    test for admin

--- software/Visual-Studio-Code-Installers/windows-vs-code-installer.ps1
+++ software/Visual-Studio-Code-Installers/windows-vs-code-installer.ps1
@@ -1,43 +1,50 @@
-$Username = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
-
 $FileUri = "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64"
 $Destination = "vscodeInstaller.exe"
 $exeArgs = '/verysilent /tasks=addcontextmenufiles,addcontextmenufolders,addtopath'
 
-$bitsJobObj = Start-BitsTransfer $FileUri -Destination $Destination
+param([switch]$Elevated)
+function Test-Admin{
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
 
-$vsCodeExec = ($Env:PROGRAMFILES) + "\Microsoft VS Code\bin\code.cmd"
-$extensions =@(
-    "ms-vscode.cpptools",               #C/C++ Language Support
-    "ms-python.python",                 #Python Language Support
-    "ms-vscode.cmake-tools",            #CMake Tools Support
-    "ms-vscode.vscode-serial-monitor",  #Serial Monitor Extension
-    "platformio.platformio-ide"         #PlatformIO Support
-) | SORT-OBJECT
+if((Test-Admin) -eq $false){
+    $bitsJobObj = Start-BitsTransfer $FileUri -Destination $Destination
 
-switch($bitsJobObj.JobState){
-    'Transferred'{
-        Complete-BitsTransfer -BitsJob $bitsJobObj
-        break
+    $vsCodeExec = ($Env:PROGRAMFILES) + "\Microsoft VS Code\bin\code.cmd"
+    $extensions =@(
+        "ms-vscode.cpptools",               #C/C++ Language Support
+        "ms-python.python",                 #Python Language Support
+        "ms-vscode.cmake-tools",            #CMake Tools Support
+        "ms-vscode.vscode-serial-monitor",  #Serial Monitor Extension
+        "platformio.platformio-ide"         #PlatformIO Support
+    ) | SORT-OBJECT
+
+    switch($bitsJobObj.JobState){
+        'Transferred'{
+            Complete-BitsTransfer -BitsJob $bitsJobObj
+            break
+        }
+        'Error'{
+            throw 'Error downloading'
+        }
     }
-    'Error'{
-        throw 'Error downloading'
+
+    Start-Process -Wait $Destination -ArgumentList $exeArgs
+
+    $extensions | ForEach-Object{
+        try{
+            Invoke-Expression "& '$vsCodeExec' --install-extension $_ --force"
+            Write-Host
+        }
+        catch{
+            $_
+            Exit(1)
+        }
     }
+
+    Exit(0)
 }
 
 
 
-Start-Process -Wait $Destination -ArgumentList $exeArgs
-
-$extensions | ForEach-Object{
-    try{
-        Invoke-Expression "& '$vsCodeExec' --install-extension $_ --force"
-        Write-Host
-    }
-    catch{
-        $_
-        Exit(1)
-    }
-}
-
-Exit(0)
