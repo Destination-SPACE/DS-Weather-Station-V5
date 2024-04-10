10b2cba68dac1e0c69d363286e23ac08da0bfb98
DeepSpace <deepspace00@pm.me>
2024-04-10 12:50:12 -0400

    fixed spelling

--- software/Visual-Studio-Code-Installers/windows-vs-code-installer.bat
+++ software/Visual-Studio-Code-Installers/windows-vs-code-installer.bat
@@ -1,21 +1,21 @@
 REM Install Microsoft Visual Studio Code
 echo Installing Microsoft Visual Studio Code...
-call winget install -e --id Microsoft.VisualStudioCode
+winget install --id=Microsoft.VisualStudioCode
 
-REM Install Microsoft Visual Studio Code Extensions
+:: Install Microsoft Visual Studio Code Extensions
 echo Installing C/C++ Extension...
-call code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.cpptools
 
 echo Installing CMake Tools Extension...
-call code --install-extension ms-vscode.cmake-tools
+code --install-extension ms-vscode.cmake-tools
 
 echo Installing PlatformIO Extension...
-call code --install-extension platformio.platformio-ide
+code --install-extension platformio.platformio-ide
 
 echo Installing Serial Monitor Extension...
-call code --install-extension ms-vscode.vscode-serial-monitor
+code --install-extension ms-vscode.vscode-serial-monitor
 
-ech Installing Python...
-call code --install-extension ms-python.python
+echo Installing Python...
+code --install-extension ms-python.python
 
 echo Installation Complete!
