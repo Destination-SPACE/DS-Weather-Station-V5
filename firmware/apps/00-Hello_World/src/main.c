463c0bf928b9868801d41a0a0643644ea4de71b6
DeepSpace00 <deepspace00@pm.me>
2025-09-03 15:31:40 -0400

    created Hello World! example

---
+++ firmware/apps/00-Hello_World/src/main.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <zephyr/kernel.h>
+
+int main(void)
+{
+    k_msleep(1000);
+	printf("Hello World!\n");
+
+	return 0;
+}
